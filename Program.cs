19ad5aa454268138accb788310c5994907046152
Daiane Gomes <dgomes_2013@hotmail.com>
2025-06-15 11:56:30 -0300

    Implementação da lógica das classes Smartphone, Nokia e iPhone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE: Realizar os testes com as classes Nokia e Iphone
+
+// Testando classe Nokia
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "ABC123XYZ", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+// Testando classe Iphone
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 14", "XYZ789ABC", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
